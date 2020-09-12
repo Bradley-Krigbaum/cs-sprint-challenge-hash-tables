fff03d1f95d3576d6c27f034f6f12387dae0e8ba
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-09-11 20:41:25 -0700

    ex5 MVP

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,22 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    paths = {}
+
+    for path in files:
+        file_name = path.split('/')[-1]
+
+        if file_name not in paths:
+            paths[file_name] = [path]
+        else:
+            paths[file_name].append(path)
+
+    result = []
+
+    for query in queries:
+        if query in paths:
+            for q_path in paths[query]:
+                result.append(q_path)
 
     return result
 
