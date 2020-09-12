7471431560ac071e7cdd97db506e61de0b10f763
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-09-11 20:17:41 -0700

    MVP ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,23 +1,19 @@
-from hashtable import HashTable
-
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-    ht = HashTable(16)
-    ht_find = HashTable.ht_find
-    ht_put = HashTable.ht_put
-
-    if len(weights) == 1:
-        return None
     
-    for weight in weights:
-        ht_put(ht, weight, weights.index(weight))
-        if ht_find(ht, (limit - weight)) is not None:
-            if weights.index(weight) == ht_find(ht, limit - weight):
-                newList = [weights.index(weight)+1, ht_find(ht, limit - weight)]
+    weight_dict = {}
+
+    for i in range(length):
+        weight_dict[weights[i]] = i
+    
+    for i in range(length):
+        weight_limit = limit - weights[i]
+        if weight_limit in weight_dict:
+            if weight_dict[weight_limit] > i:
+                return weight_dict[weight_limit], i
             else:
-                newList = [weights.index(weight), ht_find(ht, limit - weight)]
-            
-    return newList
+                return i, weight_dict[weight_limit]
+
+    return None
